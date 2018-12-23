067780f3992525ff70c8b6a06836721e0c630a5c
ettore <ettorecelozzi@gmail.com>
2018-12-23 17:52:09 +0100

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,11 @@ public class Main {
         PrintWriter writer100 = new PrintWriter(new BufferedWriter(new FileWriter("db100.txt", true)));
         PrintWriter writer500 = new PrintWriter(new BufferedWriter(new FileWriter("db500.txt", true)));
         PrintWriter writer1000 = new PrintWriter(new BufferedWriter(new FileWriter("db1000.txt", true)));
+        PrintWriter writer5000 = new PrintWriter(new BufferedWriter(new FileWriter("db5000.txt", true)));
         PrintWriter writer10000 = new PrintWriter(new BufferedWriter(new FileWriter("db10000.txt", true)));
+        PrintWriter writer50000 = new PrintWriter(new BufferedWriter(new FileWriter("db50000.txt", true)));
+        PrintWriter writer100000 = new PrintWriter(new BufferedWriter(new FileWriter("db100000.txt", true)));
+        PrintWriter writer500000 = new PrintWriter(new BufferedWriter(new FileWriter("db500000.txt", true)));
         PrintWriter writer1000000 = new PrintWriter(new BufferedWriter(new FileWriter("db1000000.txt", true)));
         for (int i = 0; i < 1000000; i++) {
             int a1 = rand.nextInt(2) + 1;
@@ -27,11 +31,11 @@ public class Main {
             int A4 = a1 == 2 ? 0 : a4;
             int M2_1 = m1 == 1 ? rand.nextInt(3) : m2;
             int M2 = M2_1 == 0 ? rand.nextInt(2) + 1 : M2_1;
-            int G1 = m1 == 0 && m2 == 2 ? rand.nextInt(2) : g1;
+            int G1 = m1 == 0 && M2 == 2 ? rand.nextInt(3) : g1;
             int G31 = g1 == 3 && ((m1 == 0 && (M2 == 1 || M2 == 3 || M2 == 5 || M2 == 7 || M2 == 8)) || (m1 == 1 && (M2 == 0 || M2 == 2))) ? rand.nextInt(2) : 0;
             int G21 = g1 == 3 ? G31 : g2;
             int G2 = g1 == 0 ? rand.nextInt(9) + 1 : G21;
-            int Gf = m1 == 0 && m2 == 2 ? rand.nextInt(9) : G2;
+            int Gf = m1 == 0 && M2 == 2 && G1 == 0 ? rand.nextInt(9) + 1 : G2;
             //System.out.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + crypter.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
             if (i < 100) {
                 writer100.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
@@ -42,8 +46,20 @@ public class Main {
             if (i < 1000) {
                 writer1000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
             }
+            if (i < 5000) {
+                writer5000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
+            }
             if (i < 10000) {
                 writer10000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
+            }
+            if (i < 50000) {
+                writer50000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
+            }
+            if (i < 100000) {
+                writer100000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
+            }
+            if (i < 500000) {
+                writer500000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
             }
             if (i < 1000000) {
                 writer1000000.println(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf + " " + Crypt.crypt(a1 + "" + A2 + "" + A3 + "" + A4 + "" + m1 + "" + M2 + "" + G1 + "" + Gf, "parallel"));
@@ -53,7 +69,11 @@ public class Main {
         writer100.close();
         writer500.close();
         writer1000.close();
+        writer5000.close();
         writer10000.close();
+        writer50000.close();
+        writer100000.close();
+        writer500000.close();
         writer1000000.close();
 
         //test crypt
